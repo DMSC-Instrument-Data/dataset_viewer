346c61b15a2962694f656c1d340d13e0573b340e
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-11-12 10:41:46 +0000

    Added steppers

--- Sliders.py
+++ Sliders.py
@@ -6,7 +6,7 @@ import holoviews as hv
 
 import sys
  
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton, QSlider, QHBoxLayout, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton, QSlider, QHBoxLayout, QGridLayout, QLabel, QSpinBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt,  QCoreApplication
  
@@ -14,6 +14,8 @@ import sys
 import time
 
 import numpy as np
+
+from Dimension import Dimension
 
 import matplotlib.pyplot as plt
 
@@ -34,20 +36,33 @@ class ApplicationWindow(QtWidgets.QMainWindow):
     
         super().__init__()
 
-        # Create a random 2D array
-        self.arr = np.random.rand(3, 8, 12)
-        
-        # print(self.arr[:,29,3])
-        
+        # Initialise dimension inforamtion
+        self.dim_names = ['A','B','C longername']
+        self.dim_sizes = [3,8,12] 
+       
         # Obtain number of dimensions in the data
-        self.n_dims = len(self.arr.shape)
+        self.n_dims = 3 
         
+        self.dims = [Dimension(self.dim_names[i],self.dim_sizes[i],i) for i in range(self.n_dims)] 
+
+        # Create a random 2D array
+        self.arr = np.random.rand(*self.dim_sizes)
+      
+        self.xarr = xr.DataArray(self.arr, coords = [d.coords for d in self.dims], dims = self.dim_names)
+        fig = self.xarr.plot()
+
+        print(type(fig))
+
+        for f in fig:
+            print(f)
+ 
         # Create a figure
         self.figure = Figure()
         
         # Create an axis object
         self.ax = self.figure.add_subplot(1,1,1)
-       
+      
+        # List for storing the index of the current X/Y axes 
         self.axes = [None, None]
  
         # Plot the random array
@@ -57,7 +72,6 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # Create a layout
         self._main = QtWidgets.QWidget()
         self.setCentralWidget(self._main)
-        # layout = QtWidgets.QVBoxLayout(self._main)
 
         # Create a FigureCanvas
         self.canvas = FigureCanvas(self.figure)
@@ -65,37 +79,45 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         layout = QGridLayout(self._main)
  
         # Add the canvas to the layout
-        layout.addWidget(self.canvas,0,0,1,3)
+        layout.addWidget(self.canvas,0,0,1,5)
         self.addToolBar(NavigationToolbar(self.canvas, self))
         
         # Create a list of horizontal sliders
         self.sliders = [None for _ in range(self.n_dims)]
+        self.steppers = [None for _ in range(self.n_dims)]
         self.buttons = []
  
-        for i in range(self.n_dims):
+        for dim_no in range(self.n_dims):
         
             # Create a slider for dimension-i
-            self.sliders[i] = self.create_slider(i)
+            self.sliders[dim_no] = self.create_slider(dim_no)
 
             # Create X and Y buttons for the slider
-            x_button, y_button = self.create_buttons(i)
+            x_button, y_button = self.create_buttons(dim_no)
 
             button_list = [x_button, y_button]
             self.buttons.append(button_list)            
 
+            dim_label = QLabel()
+            dim_label.setText(self.dim_names[dim_no])
+
+            self.steppers[dim_no] = self.create_stepper(dim_no)
+
             # Add the buttons and slider to the box
-            layout.addWidget(x_button, i+1, 0)
-            layout.addWidget(y_button, i+1, 1)
-            layout.addWidget(self.sliders[i], i+1, 2)
+            layout.addWidget(dim_label, dim_no+1, 0)
+            layout.addWidget(x_button, dim_no+1, 1)
+            layout.addWidget(y_button, dim_no+1, 2)
+            layout.addWidget(self.sliders[dim_no], dim_no+1, 3)
+            layout.addWidget(self.steppers[dim_no], dim_no+1, 4)
         
-    def create_slider(self, n_dims):
+    def create_slider(self, dim_no):
     
         sl = QSlider(Qt.Horizontal)
-        sl.valueChanged.connect(self.value_change(n_dims))
+        sl.valueChanged.connect(self.slider_change(dim_no))
       
         # Set slider values
         sl.setMinimum(0)
-        sl.setMaximum(self.arr.shape[n_dims] - 1)
+        sl.setMaximum(self.arr.shape[dim_no] - 1)
         sl.setValue(0)
         
         # Set tick interval
@@ -105,7 +127,17 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         # sl.setEnabled(False)
         
         return sl
-    
+   
+    def create_stepper(self, dim_no):
+
+        stepper = QSpinBox()
+
+        stepper.setRange(0,self.dim_sizes[dim_no]-1)
+
+        stepper.valueChanged.connect(self.stepper_change(dim_no))
+
+        return stepper
+
     def create_buttons(self, n_curr_dim):
   
         x_button = QPushButton("X")
@@ -153,10 +185,26 @@ class ApplicationWindow(QtWidgets.QMainWindow):
                 return True
         return False
  
-    def value_change(self,n_curr_dim):
+    def stepper_change(self,n_curr_dim):
    
         # totally clear names 
-        def value_changer():
+        def step_changer():
+
+            # Obtain the slider value
+            stepper_val = self.steppers[n_curr_dim].value()
+      
+            # Change slider value
+            self.sliders[n_curr_dim].setValue(stepper_val)
+
+            # Use value change function 
+            self.slider_change(n_curr_dim)()
+ 
+        return step_changer
+        
+    def slider_change(self,n_curr_dim):
+   
+        # totally clear names 
+        def slider_changer():
 
             # Obtain the slider value
             slider_val = self.sliders[n_curr_dim].value()
@@ -170,7 +218,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             # Will this work? 
             self.canvas.draw()
         
-        return value_changer
+        return slider_changer
         
 if __name__ == "__main__":
     qapp = QtWidgets.QApplication(sys.argv)
