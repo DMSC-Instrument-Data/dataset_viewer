4fb4e693d7318e71f000b2d9582685c70256d05d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-07 10:49:51 +0000

    Undoing bad coverage

--- datasetviewer/viewer/StringReverser.py
+++ datasetviewer/viewer/StringReverser.py
@@ -1,9 +1,2 @@
 def reverse_string(word):
     return word[::-1]
-def not_tested_function():
-    for i in range(20):
-        print("Hello")
-    for i in range(20):
-        print("Hello")
-    for i in range(20):
-        print("Hello")
