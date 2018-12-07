575c81d13e214480a0ecd6a62b4dbd4db873b6d4
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-07 10:14:16 +0000

    Different setup file args

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DMSC-Instrument-Data/dataset_viewer",
-    packages=setuptools.find_packages(),
+    packages=['datasetviewer'],
     tests_require=['nose>=1'],
-    test_suite='nose.collector',
+    test_suite='datasetviewer.tests',
     classifiers=[
         "Programming Language :: Python :: 3"
     ],
