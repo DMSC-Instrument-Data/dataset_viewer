0831cbd9f94734c323065bb1da73466e10e61011
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-07 10:46:53 +0000

    Coverage fail test

--- datasetviewer/viewer/StringReverser.py
+++ datasetviewer/viewer/StringReverser.py
@@ -1,2 +1,9 @@
 def reverse_string(word):
     return word[::-1]
+def not_tested_function():
+    for i in range(20):
+        print("Hello")
+    for i in range(20):
+        print("Hello")
+    for i in range(20):
+        print("Hello")
