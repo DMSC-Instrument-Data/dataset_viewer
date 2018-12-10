fd2c7ea1a6723289eca6140c68237b0f09228e7e
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-10 09:24:25 +0000

    Tests for presenter arguments during initialisation

--- datasetviewer/tests/dataset_presenter_test.py
+++ datasetviewer/tests/dataset_presenter_test.py
@@ -11,4 +11,11 @@ class PreviewPresenterTest(unittest.TestCase):
         self.view = mock.create_autospec(PreviewView)
         self.source = mock.create_autospec(PreviewSource)
 
+    def test_presenter_throws_if_view_none(self):
+        with self.assertRaises(ValueError):
+            p = PreviewPresenter(view=None, source=self.source)
+
+    def test_presenter_throws_if_source_none(self):
+        with self.assertRaises(ValueError):
+            p = PreviewPresenter(view=self.view, source=None)
 
