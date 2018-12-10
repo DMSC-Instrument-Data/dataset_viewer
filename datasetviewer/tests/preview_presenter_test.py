6e13ccd121753c7ac7983fca5dbbbbf8857c72dd
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-10 09:42:04 +0000

    Missing self in test setup

--- datasetviewer/tests/preview_presenter_test.py
+++ datasetviewer/tests/preview_presenter_test.py
@@ -5,17 +5,17 @@ from datasetviewer.preview import PreviewPresenter
 from datasetviewer.preview import PreviewView
 from datasetviewer.preview import PreviewSource
 
+
 class PreviewPresenterTest(unittest.TestCase):
 
-    def setUp():
+    def setUp(self):
         self.view = mock.create_autospec(PreviewView)
         self.source = mock.create_autospec(PreviewSource)
 
     def test_presenter_throws_if_view_none(self):
         with self.assertRaises(ValueError):
-            p = PreviewPresenter(view=None, source=self.source)
+            prev_presenter = PreviewPresenter(view=None, source=self.source)
 
     def test_presenter_throws_if_source_none(self):
         with self.assertRaises(ValueError):
-            p = PreviewPresenter(view=self.view, source=None)
-
+            prev_presenter = PreviewPresenter(view=self.view, source=None)
