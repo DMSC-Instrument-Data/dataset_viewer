48226cdb6e5e73f19c9fd472368655d3c736e32c
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-13 11:21:13 +0000

    Testing data validation for good data

--- datasetviewer/tests/file_reader_test.py
+++ datasetviewer/tests/file_reader_test.py
@@ -13,8 +13,14 @@ class FileReaderTest(unittest.TestCase):
         # Test that a file that contains an empty dictionary isn't accepted
         pass
 
-    def test_validate_data_dimensions(self):
+    def test_bad_dimensions(self):
 
         bad_data = xr.Dataset({'good': (['x', 'y', 'z'], np.random.rand(3, 4, 5)), 'bad': np.random.rand(2)})
 
         self.assertTrue(self.file_reader.invalid_dataset(bad_data))
+
+    def test_good_dimensions(self):
+
+        good_data = xr.Dataset({'good': (['x', 'y', 'z'], np.random.rand(3, 4, 5)),  'alsogood': (['a', 'b', 'c', 'e'], np.random.rand(3, 4, 5, 6))})
+
+        self.assertFalse(self.file_reader.invalid_dataset(good_data))
