afef7fffe55a3b545f18c6c27669fbdf693b85fe
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-17 15:22:54 +0000

    Removing setters from Variable

--- datasetviewer/dataset/Variable.py
+++ datasetviewer/dataset/Variable.py
@@ -8,17 +8,9 @@ class Variable(object):
     def name(self):
         return self._name
 
-    @name.setter
-    def name(self, name):
-        self._name = name
-
     @property
     def data(self):
         return self._data
-
-    @data.setter
-    def data(self, data):
-        self._data = data
 
     def get_dimensions(self):
         return self._data.shape
