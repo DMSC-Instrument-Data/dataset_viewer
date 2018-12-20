61cb2547df2d6b447b55cdd591276a4b3daa223e
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-20 10:07:24 +0000

    Renaming mocks and tests in FileLoaderPresenterTest

--- datasetviewer/tests/file_loader_presenter_test.py
+++ datasetviewer/tests/file_loader_presenter_test.py
@@ -15,17 +15,17 @@ class FileLoaderPresenterTest(unittest.TestCase):
 
     def setUp(self):
 
-        self.main_presenter = mock.create_autospec(MainViewPresenterInterface)
-        self.main_presenter.set_data = mock.MagicMock()
-        self.view = mock.create_autospec(FileLoaderView)
+        self.mock_main_presenter = mock.create_autospec(MainViewPresenterInterface)
+        self.mock_main_presenter.set_data = mock.MagicMock()
+        self.mock_view = mock.create_autospec(FileLoaderView)
 
-        self.dummy_data = xr.Dataset()
+        self.empty_data = xr.Dataset()
         self.fake_file_path = "filepath"
 
-        self.view.get_selected_file_path = mock.MagicMock(side_effect=lambda: self.fake_file_path)
+        self.mock_view.get_selected_file_path = mock.MagicMock(side_effect=lambda: self.fake_file_path)
 
-        self.fl_presenter = FileLoaderPresenter(self.view)
-        self.fl_presenter.register_master(self.main_presenter)
+        self.fl_presenter = FileLoaderPresenter(self.mock_view)
+        self.fl_presenter.register_master(self.mock_main_presenter)
 
     def test_presenter_throws_if_view_none(self):
         '''
@@ -39,41 +39,41 @@ class FileLoaderPresenterTest(unittest.TestCase):
         Test the two-way link between the FileLoaderPresenter and its MainViewPresenter by ensuring that the master's
         `subscribe_subpresenter` method is called after the FileLoaderPresenter's `register_master` method is called.
         '''
-        fl_presenter = FileLoaderPresenter(self.view)
+        fl_presenter = FileLoaderPresenter(self.mock_view)
 
         main_presenter = mock.create_autospec(MainViewPresenterInterface)
         fl_presenter.register_master(main_presenter)
 
         main_presenter.subscribe_subpresenter.assert_called_once_with(fl_presenter)
 
-    def test_file_selection_calls_get_path(self):
+    def test_file_selection_calls_get_path_on_view(self):
         '''
         Test that a FILELOADREQUEST in the FileLoaderPresenter's `notify` function causes the `get_selected_file_path`
         method on the FileLoaderVIew to be called.
         '''
-        fl_presenter = FileLoaderPresenter(self.view)
-        fl_presenter.register_master(self.main_presenter)
+        fl_presenter = FileLoaderPresenter(self.mock_view)
+        fl_presenter.register_master(self.mock_main_presenter)
         fl_presenter._load_data = mock.MagicMock()
         fl_presenter.notify(Command.FILEOPENREQUEST)
-        self.view.get_selected_file_path.assert_called_once()
+        self.mock_view.get_selected_file_path.assert_called_once()
 
     def test_bad_file_shows_message(self):
         '''
         Test that the request to open a bad file causes the view to display a message.
         '''
         with mock.patch("datasetviewer.fileloader.FileLoaderTool.open_dataset",
-                        side_effect=lambda path: self.dummy_data):
+                        side_effect=lambda path: self.empty_data):
 
             self.fl_presenter._load_data(self.fake_file_path)
-            self.view.show_reject_file_message.assert_called_once()
+            self.mock_view.show_reject_file_message.assert_called_once()
 
     def test_load_file(self):
         '''
         Test that a FILEOPENREQUEST in `notify` causes the `_load_data` method to be called with the expected argument.
         '''
-        fl_presenter = FileLoaderPresenter(self.view)
-        fl_presenter.register_master(self.main_presenter)
-        fl_presenter._load_data = mock.MagicMock(side_effect = lambda path: self.dummy_data.variables)
+        fl_presenter = FileLoaderPresenter(self.mock_view)
+        fl_presenter.register_master(self.mock_main_presenter)
+        fl_presenter._load_data = mock.MagicMock(side_effect = lambda path: self.empty_data.variables)
 
         fl_presenter.notify(Command.FILEOPENREQUEST)
         fl_presenter._load_data.assert_called_once_with(self.fake_file_path)
@@ -85,16 +85,16 @@ class FileLoaderPresenterTest(unittest.TestCase):
         :return:
         '''
         with mock.patch("datasetviewer.fileloader.FileLoaderTool.file_to_dict",
-                        side_effect = lambda path: self.dummy_data.variables):
+                        side_effect = lambda path: self.empty_data.variables):
 
             self.fl_presenter.notify(Command.FILEOPENREQUEST)
-            self.main_presenter.set_data.assert_called_once_with(self.dummy_data.variables)
+            self.mock_main_presenter.set_data.assert_called_once_with(self.empty_data.variables)
 
     def test_unknown_command_raises(self):
         '''
         Test that an exception is thrown if notify is called with a command it does not recognise.
         '''
-        fl_presenter = FileLoaderPresenter(self.view)
+        fl_presenter = FileLoaderPresenter(self.mock_view)
 
         fake_enum = Enum(value='invalid', names=[('bad_command', -200000)])
 
