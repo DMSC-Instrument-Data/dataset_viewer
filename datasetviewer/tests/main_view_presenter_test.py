80277416f15e1545d3727da40dea85c73e3fedce
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-12-20 11:04:35 +0000

    Mocking PreviewPresenter in MainViewPresenter Tests

--- datasetviewer/tests/main_view_presenter_test.py
+++ datasetviewer/tests/main_view_presenter_test.py
@@ -13,11 +13,10 @@ class MainViewPresenterTest(unittest.TestCase):
 
     def setUp(self):
 
-        self.main_view = mock.create_autospec(MainView)
-        self.source = mock.Mock()
-        self.sub_presenters = [mock.create_autospec(SubPresenter) for _ in range(10)]
-        self.preview_presenter = PreviewPresenter(mock.Mock())
-        self.preview_presenter.set_data = mock.MagicMock()
+        self.mock_main_view = mock.create_autospec(MainView)
+        self.mock_source = mock.Mock()
+        self.mock_sub_presenters = [mock.create_autospec(SubPresenter) for _ in range(10)]
+        self.mock_preview_presenter = mock.create_autospec(PreviewPresenter)
         self.fake_data = xr.Dataset().variables
 
     def test_presenter_throws_if_view_none(self):
@@ -25,26 +24,26 @@ class MainViewPresenterTest(unittest.TestCase):
         Test that the MainViewPresenter throws an Exception if the MainView is None.
         '''
         with self.assertRaises(ValueError):
-            MainViewPresenter(None, *self.sub_presenters)
+            MainViewPresenter(None, *self.mock_sub_presenters)
 
     def test_main_throws_if_sub_none(self):
         '''
         Test that the MainViewPresenter throws an Exception if any of the SubPresenters are None.
         :return:
         '''
-        badsubpresenters = self.sub_presenters + [None]
+        badsubpresenters = self.mock_sub_presenters + [None]
 
         with self.assertRaises(ValueError):
-            MainViewPresenter(self.main_view, *badsubpresenters)
+            MainViewPresenter(self.mock_main_view, *badsubpresenters)
 
     def test_constructor_success(self):
         '''
         Test that the `register_master` method is called in each of the SubPresenters when the MainViewPresenter is
         initialised.
         '''
-        main_view_presenter = MainViewPresenter(self.main_view, *self.sub_presenters)
+        main_view_presenter = MainViewPresenter(self.mock_main_view, *self.mock_sub_presenters)
 
-        for presenter in self.sub_presenters:
+        for presenter in self.mock_sub_presenters:
             presenter.register_master.assert_called_once_with(main_view_presenter)
 
     def test_set_data_to_preview_presenter(self):
@@ -52,8 +51,9 @@ class MainViewPresenterTest(unittest.TestCase):
         Test that the MainViewPresenter passes a data dictionary to the PreviewPresenter when its data attribute is set
         to a value.
         '''
-        sub_presenters = self.sub_presenters + [self.preview_presenter]
-        main_view_presenter = MainViewPresenter(self.main_view, *sub_presenters)
+        sub_presenters = self.mock_sub_presenters + [self.mock_preview_presenter]
+        main_view_presenter = MainViewPresenter(self.mock_main_view, *sub_presenters)
+        main_view_presenter.subscribe_preview_presenter(self.mock_preview_presenter)
 
         main_view_presenter.set_data(self.fake_data)
-        self.preview_presenter.set_data.assert_called_once_with(self.fake_data)
+        self.mock_preview_presenter.set_data.assert_called_once_with(self.fake_data)
