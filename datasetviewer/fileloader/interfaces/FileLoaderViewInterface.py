bb83f838f82da625357c2ab8f0953365ebf5cc49
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-01-11 12:18:07 +0000

    ABC working correctly

--- datasetviewer/fileloader/interfaces/FileLoaderViewInterface.py
+++ datasetviewer/fileloader/interfaces/FileLoaderViewInterface.py
@@ -1,7 +1,17 @@
-class FileLoaderViewInterface():
+from abc import ABCMeta, abstractmethod
+from PyQt5 import QtCore
 
+from six import with_metaclass
+
+class Meta(ABCMeta, type(QtCore.QObject)):
+    pass
+
+class FileLoaderViewInterface(with_metaclass(Meta)):
+
+    @abstractmethod
     def get_selected_file_path(self):
         pass
 
+    @abstractmethod
     def show_reject_file_message(self, error_msg):
         pass
