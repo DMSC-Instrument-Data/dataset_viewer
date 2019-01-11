7ff1c10a251077f656464bb2ecb0d4968394a039
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-01-11 15:54:26 +0000

    Failing test for PlotPresenter

--- datasetviewer/tests/main_view_presenter_test.py
+++ datasetviewer/tests/main_view_presenter_test.py
@@ -8,6 +8,10 @@ from datasetviewer.mainview.MainViewPresenter import MainViewPresenter
 from datasetviewer.mainview.interfaces.MainViewInterface import MainViewInterface
 from datasetviewer.preview.interfaces.PreviewPresenterInterface import PreviewPresenterInterface
 from datasetviewer.subpresenter.SubPresenterInterface import SubPresenterInterface
+from datasetviewer.plot.interfaces.PlotPresenterInterface import PlotPresenterInterface
+from datasetviewer.dataset.Variable import Variable
+
+from collections import OrderedDict as DataSet
 
 class MainViewPresenterTest(unittest.TestCase):
 
@@ -17,6 +21,7 @@ class MainViewPresenterTest(unittest.TestCase):
         self.mock_source = mock.Mock()
         self.mock_sub_presenters = [mock.create_autospec(SubPresenterInterface) for _ in range(10)]
         self.mock_preview_presenter = mock.create_autospec(PreviewPresenterInterface)
+        self.mock_plot_presenter = mock.create_autospec(PlotPresenterInterface)
         self.fake_data = xr.Dataset().variables
 
     def test_presenter_throws_if_view_none(self):
@@ -57,3 +62,21 @@ class MainViewPresenterTest(unittest.TestCase):
 
         main_view_presenter.set_data(self.fake_data)
         self.mock_preview_presenter.set_data.assert_called_once_with(self.fake_data)
+
+    def test_create_default_plot(self):
+        '''
+        Test that a call to the MainViewPresenter `create_default_plot` function calls another function of the same
+        name in the PlotPresenter
+        '''
+
+        main_view_presenter = MainViewPresenter(self.mock_main_view, *self.mock_sub_presenters)
+
+        # Create a fake dataset and place it in the MainViewPresenter
+        fake_data = DataSet()
+        fake_key = "fake_key"
+        fake_elem = Variable(fake_key, self.fake_data)
+        fake_data["fake_key"] = fake_elem
+        main_view_presenter._data = fake_data
+
+        main_view_presenter.create_default_plot(fake_key)
+        self.mock_plot_presenter.create_default_plot.assert_called_once_with(fake_elem)
