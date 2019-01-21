e00af300d324f5dceb3b922f213cf7fecebc5aec
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2019-01-21 11:29:28 +0000

    Further comments for FileLoaderPresenter

--- datasetviewer/tests/file_loader_presenter_test.py
+++ datasetviewer/tests/file_loader_presenter_test.py
@@ -11,6 +11,8 @@ from datasetviewer.fileloader.Command import Command
 
 import xarray as xr
 
+from collections import OrderedDict as DataSet
+
 class FileLoaderPresenterTest(unittest.TestCase):
 
     def setUp(self):
@@ -27,6 +29,9 @@ class FileLoaderPresenterTest(unittest.TestCase):
         self.fake_file_path = ("filepath", ".ext")
         self.mock_view.get_selected_file_path = mock.MagicMock(side_effect=lambda: self.fake_file_path)
 
+        # Create a fake dictionary
+        self.empty_dict = DataSet()
+
     def test_presenter_throws_if_view_none(self):
         '''
         Test that the FileLoaderPresenter throws an Exception when the FileLoaderView is None.
@@ -37,7 +42,7 @@ class FileLoaderPresenterTest(unittest.TestCase):
     def test_file_selection_calls_get_path_on_view(self):
         '''
         Test that a FILEOPENREQUEST in the FileLoaderPresenter's `notify` function causes the `get_selected_file_path`
-        method on the FileLoaderVIew to be called.
+        method on the FileLoaderView to be called.
         '''
         fl_presenter = FileLoaderPresenter(self.mock_view)
         fl_presenter.register_master(self.mock_main_presenter)
@@ -63,31 +68,41 @@ class FileLoaderPresenterTest(unittest.TestCase):
 
             fl_presenter.notify(Command.FILEOPENREQUEST)
 
-            # Check that loading a bad dataset caused a message to be displayed on the view indicating that the file
-            # was rejected
-
+            '''
+            Check that loading a bad dataset caused a message to be displayed on the view indicating that the file
+            was rejected.
+            '''
             self.mock_view.show_reject_file_message.assert_called_once()
 
-            # Check that the `set_dict` method in the MainPresenter was not called as a result of the file being
-            # rejected
+            '''
+            Check that the `set_dict` method in the MainViewPresenter was not called as a result of the file being
+            rejected.
+            '''
             self.mock_main_presenter.set_dict.assert_not_called()
 
     def test_closing_filedialog_does_nothing(self):
         '''
         Test that closing the FileDialog without choosing a file causes the presenter to return without attempting
-        to open or interpret a file
+        to open/interpret a file.
         '''
 
         mock_view = mock.create_autospec(FileLoaderViewInterface)
 
-        # Tell the mock to behave as if the FileDialog was closed without a file being chosen
+        '''
+        Tell the mock FileLoaderView to behave as if the FileDialog was closed without a file being chosen by returning
+        an empty "filename"
+        '''
         mock_view.get_selected_file_path = mock.MagicMock(side_effect = lambda: ('',''))
 
         fl_presenter = FileLoaderPresenter(mock_view)
         fl_presenter.register_master(self.mock_main_presenter)
+
+        # Mock the `_load_data` method to check its calls
         fl_presenter._load_data = mock.MagicMock()
 
         fl_presenter.notify(Command.FILEOPENREQUEST)
+
+        # Check that the `_load_data` and `set_dict` methods were not called due to a file not being selected
         fl_presenter._load_data.assert_not_called()
         self.mock_main_presenter.set_dict.assert_not_called()
 
@@ -97,9 +112,16 @@ class FileLoaderPresenterTest(unittest.TestCase):
         '''
         fl_presenter = FileLoaderPresenter(self.mock_view)
         fl_presenter.register_master(self.mock_main_presenter)
-        fl_presenter._load_data = mock.MagicMock(side_effect = lambda path: self.empty_data.variables)
+
+        # Mock the `_load_data` method
+        fl_presenter._load_data = mock.MagicMock()
 
         fl_presenter.notify(Command.FILEOPENREQUEST)
+
+        '''
+        Check that the `_load_data` method was called with the first element from the filename-extension tuple that gets
+        returned by the FileLoaderView.
+        '''
         fl_presenter._load_data.assert_called_once_with(self.fake_file_path[0])
 
     def test_file_open_success_informs_main_presenter(self):
@@ -111,11 +133,14 @@ class FileLoaderPresenterTest(unittest.TestCase):
         fl_presenter = FileLoaderPresenter(self.mock_view)
         fl_presenter.register_master(self.mock_main_presenter)
 
+        # Mock the `file_to_dict` function from the FileLoaderTool and make it return a fake dictionary
         with mock.patch("datasetviewer.fileloader.FileLoaderTool.file_to_dict",
-                        side_effect = lambda path: self.empty_data.variables):
+                        side_effect = lambda path: self.empty_dict):
 
             fl_presenter.notify(Command.FILEOPENREQUEST)
-            self.mock_main_presenter.set_dict.assert_called_once_with(self.empty_data.variables)
+
+            # Check that the `set_dict` function in the MainViewPresenter was called with the same dictionary
+            self.mock_main_presenter.set_dict.assert_called_once_with(self.empty_dict)
 
     def test_notify_raises_if_command_unknown(self):
         '''
@@ -123,6 +148,7 @@ class FileLoaderPresenterTest(unittest.TestCase):
         '''
         fl_presenter = FileLoaderPresenter(self.mock_view)
 
+        # Create a fake command/enum
         fake_enum = Enum(value='invalid', names=[('bad_command', -200000)])
 
         with self.assertRaises(ValueError):
